9d5136523cc5514ac3fc1187a94110193d00df18
Nguyễn Quang Mẫn <1150080065@sv.hcmunre.edu.vn>
2025-10-18 12:19:48 +0700

    Delete banlinhkienmaytinh/quanlybanlinhkienmaytinh directory

--- banlinhkienmaytinh/quanlybanlinhkienmaytinh/src/main/java/com/example/quanlybanlinhkienmaytinh/services/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.quanlybanlinhkienmaytinh.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.quanlybanlinhkienmaytinh.models.Product;
-
-public interface ProductsRepository extends JpaRepository<Product , Integer> {
-}
